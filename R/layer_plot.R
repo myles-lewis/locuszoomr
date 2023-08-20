e1f9c3a71f7ca958f385d140938728e1cbff25f2
Myles Lewis <myles.lewis@qmul.ac.uk>
2023-08-20 12:04:23 +0100

    Shift plot to do.call

--- R/layer_plot.R
+++ R/layer_plot.R
@@ -86,25 +86,43 @@ scatter_plot <- function(x,
   
   # scatter plot
   if (align) {
-    op <- par(tcl = -0.25, las = 1, font.main = 1,
+    op <- par(tcl = -0.25,
               mar = c(ifelse(xticks, 3, 0.1), 4, 2, 1.5),
               mgp = c(1.7, 0.5, 0))
     on.exit(par(op))
   }
   
-  plot(data[, x$pos], data$logP,
-       pch = 21, bg = data$col,
-       xlim = x$xrange,
-       xlab = if (xticks) xlab else "",
-       ylab = ylab,
-       bty = if (border) 'o' else 'l',
-       cex.axis = cex.axis,
-       xaxt = 'n',
-       panel.first = {
-         if (!is.null(pcutoff)) {
-           abline(h = -log10(pcutoff), col = 'darkgrey', lty = 2)
-         }
-       }, ...)
+  new.args <- list(...)
+  plot.args <- list(x = data[, x$pos], y = data$logP,
+               pch = 21, bg = data$col,
+               las = 1, font.main = 1,
+               xlim = x$xrange,
+               xlab = if (xticks) xlab else "",
+               ylab = ylab,
+               bty = if (border) 'o' else 'l',
+               cex.axis = cex.axis,
+               xaxt = 'n',
+               panel.first = {
+                 if (!is.null(pcutoff)) {
+                   abline(h = -log10(pcutoff), col = 'darkgrey', lty = 2)
+                 }
+               })
+  if (length(new.args)) plot.args[names(new.args)] <- new.args
+  do.call("plot", plot.args)
+  
+  # plot(data[, x$pos], data$logP,
+  #      pch = 21, bg = data$col,
+  #      xlim = x$xrange,
+  #      xlab = if (xticks) xlab else "",
+  #      ylab = ylab,
+  #      bty = if (border) 'o' else 'l',
+  #      cex.axis = cex.axis,
+  #      xaxt = 'n',
+  #      panel.first = {
+  #        if (!is.null(pcutoff)) {
+  #          abline(h = -log10(pcutoff), col = 'darkgrey', lty = 2)
+  #        }
+  #      }, ...)
   if (xticks) {
     par(mgp = c(1.6, 0.3, 0))
     axis(1, at = axTicks(1), labels = axTicks(1) / 1e6, cex.axis = cex.axis)
