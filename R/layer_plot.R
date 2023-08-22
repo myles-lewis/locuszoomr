fdb1c9c0d92e329a32f3be242e51bd2d276fefd0
Myles Lewis <myles.lewis@qmul.ac.uk>
2023-08-22 21:35:46 +0100

    Add rev to set_layers

--- R/layer_plot.R
+++ R/layer_plot.R
@@ -6,6 +6,8 @@
 #' @param n Number of plots (not including gene tracks on bottom)
 #' @param heights Vector of length `nrow + 1` specifying height for plots with
 #'   a gene track on the bottom
+#' @param rev Logical whether to reverse plotting order and plot from bottom to 
+#'   top
 #' @return Sets [layout()] to enable multiple plots aligned in a column. The
 #'   gene track is assumed to be positioned on the bottom. Returns `par()`
 #'   invisibly so that layout can be reset to default at the end of plotting.
@@ -13,9 +15,11 @@
 #' @export
 
 set_layers <- function(n = 1,
-                       heights = c(rep(3, n), 2)) {
+                       heights = c(rep(3, n), 2),
+                       rev = FALSE) {
   op <- par(no.readonly = TRUE)
-  mat <- matrix(seq_len(n +1))
+  s <- if (rev) rev(seq_len(n +1)) else seq_len(n +1)
+  mat <- matrix(s)
   graphics::layout(mat, heights = heights)
   invisible(op)
 }
