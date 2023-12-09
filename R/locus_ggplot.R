d701d1592e5eb1e62c06699a2e5e51bc8a7f1f1f
Myles Lewis <myles.lewis@qmul.ac.uk>
2023-12-09 12:56:09 +0000

    Initial ggplot2 switchover

--- R/locus_ggplot.R
+++ R/locus_ggplot.R
@@ -4,7 +4,7 @@
 #' Genomic locus plot similar to locuszoom.
 #'
 #' @param loc Object of class 'locus' to use for plot. See [locus()].
-#' @param height A unit object specifying height of the lower gene track.
+#' @param heights Vector supplying the ratio of top to bottom plot.
 #' @param index_snp Specifies index SNP to be shown in a different colour and
 #'   symbol. Defaults to the SNP with the lowest p-value. Set to `NULL` to not
 #'   show this.
@@ -31,7 +31,6 @@
 #'   the index SNP.
 #' @param legend_pos Position of legend e.g. "topleft", "topright" or ggplot2
 #'   settings. Set to `NULL` to hide legend.
-#' @param draw Logical whether to call [grid.draw()] to draw the plot.
 #' @param ... Additional arguments passed to [gg_genetracks()] to control
 #'   colours of gene tracks etc.
 #' @return Returns a ggplot2 plot containing a scatter plot with genetracks
@@ -44,12 +43,10 @@
 #'              ens_db = "EnsDb.Hsapiens.v75")
 #' locus_ggplot(loc)
 #' }
-#' @importFrom ggplot2 ggplotGrob find_panel
-#' @importFrom gtable gtable_add_rows gtable_add_grob
-#' @importFrom grid grid.newpage grid.draw
+#' @importFrom cowplot plot_grid
 #' @export
 
-locus_ggplot <- function(loc, height = unit(5, "cm"),
+locus_ggplot <- function(loc, heights = c(3, 2),
                          index_snp = loc$index_snp,
                          pcutoff = 5e-08,
                          scheme = c('royalblue', 'red', 'purple'),
@@ -64,7 +61,6 @@ locus_ggplot <- function(loc, height = unit(5, "cm"),
                          LD_scheme = c('grey', 'royalblue', 'cyan2', 'green3', 
                                        'orange', 'red', 'purple'),
                          legend_pos = 'topleft',
-                         draw = TRUE,
                          ...) {
   p <- gg_scatter(loc,
                   index_snp = index_snp,
@@ -80,19 +76,10 @@ locus_ggplot <- function(loc, height = unit(5, "cm"),
                   showLD = showLD,
                   LD_scheme = LD_scheme,
                   legend_pos = legend_pos)
-  g <- ggplotGrob(p)
-  panels_extent <- g %>% find_panel()
-  pg <- g %>%
-    gtable_add_rows(heights = height) %>%
-    gtable_add_grob(gg_genetracks(loc, xticks = (xticks != "top"),
-                                  border = border, xlab = xlab,
-                                  cex.axis = cex.axis,
-                                  cex.lab = cex.lab, draw = FALSE, ...),
-                    t = -1, b = -1,
-                    l = panels_extent$l, r = panels_extent$l +1)
-  if (draw) {
-    grid.newpage()
-    grid.draw(pg)
-  }
-  invisible(pg)
+  g <- gg_genetracks(loc, xticks = (xticks != "top"),
+                     border = border, xlab = xlab,
+                     cex.axis = cex.axis,
+                     cex.lab = cex.lab, ...)
+  
+  plot_grid(p, g, nrow = 2, rel_heights = heights, align = "v")
 }
