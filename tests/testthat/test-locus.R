9a0970b851447d58aff8237581ece75ae71c75e8
Thomas Willis <tw395@cam.ac.uk>
2023-12-10 19:52:02 +0000

    Added test for no TX case

--- tests/testthat/test-locus.R
+++ tests/testthat/test-locus.R
@@ -5,3 +5,14 @@ test_that("Error is raised when locus has no SNPs", {
 
   expect_error(locus(data = SLE_gwas_sub, ens_db = 'EnsDb.Hsapiens.v75', labs = 'rsid', seqname = 3, xrange = c(2e6, 100+2e6)), 'Locus contains no SNPs/datapoints')
 })
+
+test_that("locus function exits successfully when no transcripts are found", {
+  load(file = system.file('data', 'no_tx_data.rdata', package='locuszoomr', mustWork = T))
+  
+  library(EnsDb.Hsapiens.v86)
+  
+  loc <- locus(data = no_tx_data, chrom = 'chrom', pos = 'pos', p = 'p', labs = 'snp', seqname = 6, xrange = c(29833417, 29841260), ens_db = 'EnsDb.Hsapiens.v86')
+ 
+  # Really just wanting the locus function to exit successfully, but need an assertion 
+  expect_equal(class(loc), 'locus')  
+})
