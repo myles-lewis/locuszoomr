26b780de6a84f6bbefbf5d0108ebeb7d69aa70c2
Myles Lewis <myles.lewis@qmul.ac.uk>
2023-12-15 14:53:18 +0000

    Create quick_peak.R

---
+++ R/quick_peak.R
@@ -0,0 +1,73 @@
+
+#' Fast peak finder in GWAS data
+#' 
+#' Simple but fast function for finding peaks in genome-wide association study
+#' (GWAS) data based on fixed region size.
+#' 
+#' @param data GWAS dataset (data.frame or data.table)
+#' @param npeaks Number of peaks to find. If set to NA, algorithm finds all
+#'   distinct peaks separated from one another by region size specified by
+#'   `span`.
+#' @param p_cutoff Specifies cut-off for p-value significance above which peaks
+#'   are ignored.
+#' @param span Minimum genomic distance between peaks
+#' @param chrom Determines which column in `data` contains chromosome
+#'   information. If `NULL` tries to autodetect the column.
+#' @param pos Determines which column in `data` contains position information.
+#'   If `NULL` tries to autodetect the column.
+#' @param p Determines which column in `data` contains SNP p-values. If `NULL`
+#'   tries to autodetect the column.
+#' @returns Vector of row indices
+#' @export
+
+quick_peak <- function(data, npeaks = NA, p_cutoff = 5e-08, span = 1e7,
+                       chrom = NULL, pos = NULL, p = NULL) {
+  start <- Sys.time()
+  # autodetect column headings
+  if (is.null(chrom)) {
+    w <- grep("chr", colnames(data), ignore.case = TRUE)
+    if (length(w) == 1) {
+      chrom <- colnames(data)[w]
+    } else stop("unable to autodetect chromosome column")
+  }
+  if (is.null(pos)) {
+    w <- grep("pos", colnames(data), ignore.case = TRUE)
+    if (length(w) == 1) {
+      pos <- colnames(data)[w]
+    } else stop("unable to autodetect SNP position column")
+  }
+  if (is.null(p)) {
+    if ("p" %in% colnames(data)) {
+      p <- "p"
+    } else {
+      w <- grep("^p?val", colnames(data), ignore.case = TRUE)
+      if (length(w) == 1) {
+        p <- colnames(data)[w]
+      } else stop("unable to autodetect p-value column")
+    }
+  }
+  if (is.na(npeaks)) npeaks <- Inf
+  
+  i <- 2
+  index <- order(data[, p])
+  index <- index[data[index, p] < p_cutoff]
+  pks <- index[1]
+  del <- which(abs(data[pks, pos] - data[index, pos]) <= span &
+                 data[pks, chrom] == data[index, chrom])[-1]
+  if (length(del) > 0) index <- index[-del]
+  while (length(pks) < npeaks & i <= length(index)) {
+    if (all(abs(data[index[i], pos] - data[pks, pos]) > span |
+            data[index[i], chrom] != data[pks, chrom])) {
+      pks <- c(pks, index[i])
+      del <- which(abs(data[index[i], pos] - data[index, pos]) <= span & 
+                     data[index[i], chrom] == data[index, chrom])[-1]
+      if (length(del) > 0) index <- index[-del]
+    }
+    i <- i + 1
+  }
+  end <- Sys.time()
+  if (is.infinite(npeaks)) {
+    message(length(pks), " peaks found (", format(end - start, digits = 3), ")")
+  }
+  pks
+}
