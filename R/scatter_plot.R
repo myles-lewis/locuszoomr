f3467172157e4f6b72ce72a5ff05c6d10180002c
Myles Lewis <myles.lewis@qmul.ac.uk>
2026-09-11 09:20:51 +0100

    Blank plot if no points in scatter_plot

--- R/scatter_plot.R
+++ R/scatter_plot.R
@@ -91,9 +91,24 @@ scatter_plot <- function(loc,
                          add = FALSE,
                          align = TRUE, ...) {
   if (!inherits(loc, "locus")) stop("Object of class 'locus' required")
-  if (is.null(loc$data)) stop("No data points, only gene tracks")
-  
   .call <- match.call()
+  
+  recomb <- !is.null(loc$recomb) & !is.na(recomb_col)
+  if (align) {
+    op <- par(mar = c(ifelse(xticks, 3, 0.1), 3.5, 2,
+                      ifelse(recomb, 3.5, 1.5)))
+    on.exit(par(op))
+  }
+  
+  if (is.null(loc$data)) {
+    message("No data points")
+    # blank plot
+    plot(NA, xlim = c(0, 1), ylim = c(0, 1),
+         xaxt = "n", yaxt = "n", bty = "n", xlab = "", ylab = "")
+    text(0.5, 0.5, "No data points", adj = 0.5)
+    return(invisible())
+  }
+  
   data <- loc$data
   if (is.null(xlab)) xlab <- paste("Chromosome", loc$seqname, "(Mb)")
   if (is.null(ylab)) {
@@ -129,13 +144,6 @@ scatter_plot <- function(loc,
   }
   
   # scatter plot
-  recomb <- !is.null(loc$recomb) & !is.na(recomb_col)
-  if (align) {
-    op <- par(mar = c(ifelse(xticks, 3, 0.1), 3.5, 2,
-                      ifelse(recomb, 3.5, 1.5)))
-    on.exit(par(op))
-  }
-  
   if (is.null(ylim)) {
     ylim <- range(data[, loc$yvar], na.rm = TRUE)
     if (yzero & is.null(ylim)) ylim[1] <- min(c(0, ylim[1]))
